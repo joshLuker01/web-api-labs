c0a81086a8a3e11f52188bb6eb631a60b7cbd3e2
Josh <joshluker99@gmail.com>
2026-05-15 02:15:53 +0100

    test

---
+++ tasky-api/api/users/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import User from './userModel.js';
+import asyncHandler from 'express-async-handler';
+
+
+const router = express.Router(); 
+
+// Get all users
+router.get('/', async (req, res) => {
+    const users = await User.find();
+    res.status(200).json(users);
+});
+
+// register(Create) User
+router.post('/', async (req, res) => {
+    await User(req.body).save();
+    res.status(201).json({
+        code: 201,
+        msg: 'Successful created new user.',
+    });
+});
+export default router;
+    
--- tasky-api/index.js
+++ tasky-api/index.js
@@ -6,6 +6,9 @@ import tasksRouter from './api/tasks';
 import { connectDB } from './db';
 // other imports
 import cors from 'cors';
+//... other imports
+import usersRouter from './api/users/index.js';
+
 
 
 
@@ -29,6 +32,9 @@ app.use(cors());
 app.use(express.static('public'));
 app.use(express.json());
 app.use('/api/tasks', tasksRouter);
+//Users router
+app.use('/api/users', usersRouter);
+
 
 app.use(errHandler);
 
