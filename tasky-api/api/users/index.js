2d1c6bdc3236afd5a07d2d9ee313c835e590b70b
Josh <joshluker99@gmail.com>
2026-05-15 15:48:31 +0100

    Register and Authenticate Users

--- tasky-api/api/users/index.js
+++ tasky-api/api/users/index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import User from './userModel.js';
+import asyncHandler from 'express-async-handler';
+
 
 const router = express.Router();
 
@@ -8,14 +10,24 @@ router.get('/', async (req, res) => {
   res.status(200).json(users);
 });
 
-router.post('/', async (req, res) => {
-  const user = await User(req.body).save();
+// register(Create)/Authenticate User
+router.post('/', asyncHandler(async (req, res) => {
+    if (req.query.action === 'register') {  // if action is 'register' then save to DB
+        await User(req.body).save();
+        res.status(201).json({
+            code: 201,
+            msg: 'Successful created new user.',
+        });
+    }
+    else {  // Must be an authenticate then!!! Query the DB and check if there's a match
+        const user = await User.findOne(req.body);
+        if (!user) {
+            return res.status(401).json({ code: 401, msg: 'Authentication failed' });
+        }else{
+            return res.status(200).json({ code: 200, msg: "Authentication Successful", token: 'TEMPORARY_TOKEN' });
+        }
+    }
+}));
 
-  res.status(201).json({
-    code: 201,
-    msg: 'Successfully created new user.',
-    user,
-  });
-});
 
 export default router;
