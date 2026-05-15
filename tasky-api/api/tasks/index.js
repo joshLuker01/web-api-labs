1c78bc7f03f22fcb4a9e8bdd262f897863a33cb0
Josh <joshluker99@gmail.com>
2026-05-15 20:53:18 +0100

    Tasks related to User - lab completed!

--- tasky-api/api/tasks/index.js
+++ tasky-api/api/tasks/index.js
@@ -6,10 +6,13 @@ const router = express.Router(); // eslint-disable-line
 
 // Get all tasks
 // Get all tasks
+// Get a user's tasks
 router.get('/', async (req, res) => {
-    const tasks = await Task.find().populate('userId', 'username');
+    console.log(req.user);
+    const tasks = await Task.find({ userId: `${req.user._id}`});
     res.status(200).json(tasks);
 });
+
 
 
 
@@ -23,9 +26,12 @@ router.get('/user/:uid', async (req, res) => {
 
 // create a task
 router.post('/', asyncHandler(async (req, res) => {
-    const task = await Task(req.body).save();
+    const newTask = req.body;
+    newTask.userId = req.user._id;
+    const task = await Task(newTask).save();
     res.status(201).json(task);
 }));
+
 
 
 
