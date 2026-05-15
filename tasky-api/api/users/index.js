ef210885961136c4772140dbf7897d32cc78d60c
Josh <joshluker99@gmail.com>
2026-05-15 16:51:43 +0100

    Authentication lab - exercise completed

--- tasky-api/api/users/index.js
+++ tasky-api/api/users/index.js
@@ -3,16 +3,12 @@ import User from './userModel.js';
 import asyncHandler from 'express-async-handler';
 import jwt from 'jsonwebtoken';
 
-
-
 const router = express.Router();
 
 router.get('/', async (req, res) => {
   const users = await User.find();
   res.status(200).json(users);
 });
-
-//.... code as before
 
 // register(Create)/Authenticate User
 router.post('/', asyncHandler(async (req, res) => {
@@ -26,7 +22,6 @@ router.post('/', asyncHandler(async (req, res) => {
             await authenticateUser(req, res);
         }
     } catch (error) {
-        // Log the error and return a generic error message
         console.error(error);
         res.status(500).json({ success: false, msg: 'Internal server error.' });
     }
@@ -34,10 +29,19 @@ router.post('/', asyncHandler(async (req, res) => {
 
 // ... Code as before
 
-
 async function registerUser(req, res) {
-    // Add input validation logic here
+
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+    if (!passwordRegex.test(req.body.password)) {
+        return res.status(400).json({
+            success: false,
+            msg: "Password must be at least 8 characters long and include uppercase, lowercase, number, and special character."
+        });
+    }
+
     await User.create(req.body);
+
     res.status(201).json({ success: true, msg: 'User successfully created.' });
 }
 
